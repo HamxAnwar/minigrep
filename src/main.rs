087eee467aff6c04e1b7f2428cd6cdd72ac654a9
HamxAnwar <hamzaanwar93@outlook.com>
2024-03-17 20:11:33 +0500

    minigrep_RUSTlang

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use std::fs;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let argument = Arguments::parse_arguments(&args);
+    
+    println!("Querry: {:?} - Filename: {:?}", argument.querry, argument.filename);
+
+    if let Err(e) = read_file(argument){
+        println!("Error: {}", e)
+    }
+}
+
+struct Arguments {
+    querry: String,
+    filename: String
+}
+
+impl Arguments {
+    fn parse_arguments(args: &[String]) -> Arguments {
+        let querry = args[1].clone();
+        let filename = args[2].clone();
+        Arguments { querry, filename }
+    }
+}
+
+
+fn read_file(argument: Arguments) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(argument.filename)?;
+    println!("{:?}", contents);
+    Ok(())
 }
